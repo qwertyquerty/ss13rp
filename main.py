261b94609e83abb33d67b44d99baa8b0705352e3
qwerty <qwertytrogi@gmail.com>
2018-06-11 22:26:40 -0500

    made things EVEN MORE confusing, fixed a bug that allowed any byond game

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import win32process
 import psutil
 import sys
 import util
+import requests
 import time
 
 if "join" in sys.argv:
@@ -30,31 +31,31 @@ else:
 
 
     servers = {
-        "Goonstation #2": ["Goonstation #2", "goonhub", "goon2.goonhub.com", 26200],
-        "Goonstation RP #1": ["Goonstation RP #1", "goonhub", "goon1.goonhub.com", 26100],
+        "Goonstation #2": ["Goonstation #2", "goonhub", "goon2.goonhub.com", 26200, "fetch"],
+        "Goonstation RP #1": ["Goonstation RP #1", "goonhub", "goon1.goonhub.com", 26100, "fetch"],
         "Yogstation 13 [99% LAGFREE!]": ["Yogstation 13", "yogstation"],
-        "BeeStation 13": ["BeeStation", "beestation", "198.58.107.171", 3333],
+        "BeeStation 13": ["BeeStation", "beestation", "198.58.107.171", 3333, "fetch"],
         "ss13": ["Unknown Server", "ss13"],
         "Oracle Station | Medium RP": ["Oracle Station", "oraclestation"], #"byond.oraclestation.com", 5000
         "Hippie Station": ["Hippie Station", "hippiestation"],
-        "/tg/Station Bagil": ["Station Bagil", "tgstation", "bagil.aws.tgstation13.org", 2337],
-        "/tg/Station Sybil": ["Station Sybil", "tgstation", "sybil.aws.tgstation13.org", 1337],
-        "/tg/Station Terry": ["Station Terry", "tgstation", "terry.tgstation13.org", 3337],
+        "/tg/Station Bagil": ["Station Bagil", "tgstation", "bagil.aws.tgstation13.org", 2337, "fetch"],
+        "/tg/Station Sybil": ["Station Sybil", "tgstation", "sybil.aws.tgstation13.org", 1337, "fetch"],
+        "/tg/Station Terry": ["Station Terry", "tgstation", "terry.tgstation13.org", 3337, "fetch"],
         "[99% FREE LAG] Convict Conclave": ["Convict Conclave", "ss13"],
         "[ss13.ru] Yellow Circus": ["Yellow Circus", "ss13"],
         "Persistence Station 13": ["Persistence Station", "persistence"],
         "Apollo Gaming": ["Apollo Gaming","apollo"],
         "Lebensraum Alpha": ["Lebensraum Alpha", "ss13"],
-        "Baystation 12": ["Baystation 12", "bs12"],
-        "FTL13": ["FTL13", "ftl13", "ftl13.com", 7777],
+        "Baystation 12": ["Baystation 12", "bs12", "https://baystation12.net/status.dat", 80, "http"],
+        "FTL13": ["FTL13", "ftl13", "ftl13.com", 7777, "fetch"],
         "Tool Box Station": ["Toolbox Station", "toolbox"],
         "Pressurized Roleplay": ["Pressurized Roleplay", "pressure"],
         "StarTrek13": ["Star Trek 13", "startrek"],
         "Polaris Station 13": ["Polaris Station", "polaris"],
         "Paradise Station 13": ["Paradise Station", "paradise"],
         "Aurora Station": ["Aurora Station", "ss13"],
-        "VOREStation": ["VOREStation", "citadel", "citadel-station.net", 44150],
-        "Citadel Station 13": ["Citadel Station", "citadel", "citadel-station.net", 44130]
+        "VOREStation": ["VOREStation", "citadel", "citadel-station.net", 44150, "fetch"],
+        "Citadel Station 13": ["Citadel Station", "citadel", "citadel-station.net", 44130, "fetch"]
     }
 
 
@@ -93,21 +94,28 @@ else:
 
                         return servers[i]
 
-        else:
-            server = "ss13"
-            return servers[server]
+            else:
+                server = "ss13"
+                return servers[server]
 
 
     while True:
         try:
             server = get_server()
             print(server)
-            if len(server) == 4:
+            if len(server) == 5:
                 try:
-                    status = util.fetch(server[2], server[3], "status")
+                    if server[4] == "fetch":
+                        status = util.fetch(server[2], server[3], "status")
+                    elif server[4] == "http":
+                        status = requests.get(server[2]).json()
                     #print(status)
-                    details = status["map_name"]+" | "+str(status["players"])+" players"
-                    if server[0] in ["Goonstation #2","Goonstation RP #1"]:
+                    if server[0] in ["Baystation 12"]:
+                        details = status["map"]+" | "+str(status["players"])+" players"
+                    elif server[0] in ["Goonstation #2","Goonstation RP #1"]:
+                        details = status["map_name"]+" | "+str(status["players"])+" players"
+
+                    if server[0] in ["Goonstation #2","Goonstation RP #1", "BeeStation", "FTL13", "Station Bagil", "Station Terry", "Station Sybil", "Citadel Station"]:
                         if status["shuttle_time"] != 'welp' and status["shuttle_time"] != '600':
                             rp.set_activity(state=server[0],details=details,large_text=server[0],large_image=server[1], start=int(time.time())-int(status["elapsed"]), end=int(time.time())+int(status["shuttle_time"]))
                         else:
